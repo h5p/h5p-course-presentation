b692a6c1111aabb841636590af545f08e6277306
Frode Petterson <frode.petterson@amendor.com>
2013-03-21 16:46:17 +0100

    Added keywords.

--- Scripts/cp.js
+++ Scripts/cp.js
@@ -19,25 +19,71 @@ H5P.CoursePresentation = function (params, id) {
  * @returns {undefined}
  */
 H5P.CoursePresentation.prototype.attach = function ($container) {
-  var slides = '';
-  for (var i = 0; i < this.slides.length +1; i++) {
-    slides += '<div class="slide';
+  $container.html('<div class="wrapper"><div class="slides-wrapper animate"></div><div class="keywords"></div></div>');
+  var $wrapper = $container.children('.wrapper');
+  this.$slidesWrapper = $wrapper.children('.slides-wrapper');
+  this.$keywords = $wrapper.children('.keywords');
+  var keywords = '';
+  
+  for (var i = 0; i < this.slides.length; i++) {
+    var slide = this.slides[i];
+    var $slide = H5P.jQuery('<div class="slide"></div>').appendTo(this.$slidesWrapper);
+    
     if (i === 0) {
-      slides += ' current';
+      this.$current = $slide.addClass('current');
     }
-    slides += '">Slide ' + i + ' <a href="#">CLICK</a></div>';
+    
+    if (slide.elements !== undefined && slide.elements.length) {
+      for (var j = 0; j < slide.elements.length; j++) {
+        var element = slide.elements[j];
+        var elementInstance = new (H5P.classFromName(element.action.library.split(' ')[0]))(element.action.params);
+        elementInstance.appendTo($slide, element.width, element.height, element.x, element.y);
+      }
+    }
+    
+    keywords += this.keywordsHtml(slide.keywords);
   }
   
-  $container.html('<div class="wrapper animate">' + slides + '</div>');
-  this.$wrapper = $container.children('.wrapper');
-  this.$slides = this.$wrapper.children('.slide');
-  this.$current = this.$slides.filter('.current');
+  this.$slides = this.$slidesWrapper.children();
+  if (keywords) {
+    this.$keywords.html('<ol>' + keywords + '</ol>');
+  }
 
   // Initialize key events
   this.initKeyEvents();
   
   // Initialize touch events
   this.initTouchEvents();
+};
+
+/**
+ * Generate html for keywords.
+ * 
+ * @param {type} keywords
+ * @returns {String}
+ */
+H5P.CoursePresentation.prototype.keywordsHtml = function (keywords) {
+  var html = '';
+  
+  if (keywords !== undefined && keywords.length) {
+    for (var i = 0; i < keywords.length; i++) {
+      var keyword = keywords[i];
+      html += '<li><span>' + keyword.main + '</span>';
+      if (keyword.subs !== undefined && keyword.subs.length) {
+        html += '<ol>';
+        for (var k = 0; k < keyword.subs.length; k++) {
+          html += '<li>' + keyword.subs[k] + '</li>';
+        }
+        html += '</ol>';
+      }
+      html += '</li>';
+    }
+    if (html) {
+      html = '<ol>' + html + '</ol>';
+    }
+  }
+  
+  return '<li>' + html + '</li>';
 };
 
 /**
@@ -82,9 +128,9 @@ H5P.CoursePresentation.prototype.initTouchEvents = function () {
   var that = this;
   var startX, startY, lastX, prevX, nextX, scroll;
  
-  this.$wrapper.bind('touchstart', function (event) {
+  this.$slidesWrapper.bind('touchstart', function (event) {
     // Disable animations when touching
-    that.$wrapper.removeClass('animate');
+    that.$slidesWrapper.removeClass('animate');
     
     // Set start positions
     startX = event.originalEvent.touches[0].pageX;
@@ -129,7 +175,7 @@ H5P.CoursePresentation.prototype.initTouchEvents = function () {
     
   }).bind('touchend', function () {
     // Enable animations again
-    that.$wrapper.addClass('animate');
+    that.$slidesWrapper.addClass('animate');
     
     if (!scroll) {
       // If we're not scrolling detemine if we're changing slide
