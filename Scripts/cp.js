593826782a14e0fd6080c0c9df256ddb26b71535
Frode Petterson <frode.petterson@amendor.com>
2013-04-02 10:58:18 +0200

    Added more example slides. Fixed jump to. Added iPad fix. Changed css nameing scheme. Other minor changes.

--- Scripts/cp.js
+++ Scripts/cp.js
@@ -19,36 +19,46 @@ H5P.CoursePresentation = function (params, id) {
  * @returns {undefined}
  */
 H5P.CoursePresentation.prototype.attach = function ($container) {
-  $container.html('<div class="wrapper"><div class="slides-wrapper animate"></div><div class="keywords"></div></div>');
-  var $wrapper = $container.children('.wrapper');
-  this.$slidesWrapper = $wrapper.children('.slides-wrapper');
-  this.$keywords = $wrapper.children('.keywords');
+  $container.addClass('h5p-course-presentation').html('<div class="h5p-wrapper"><div class="h5p-presentation-wrapper"><div class="h5p-slides-wrapper h5p-animate"></div><div class="h5p-keywords-wrapper"></div></div><div class="h5p-slideination"><a href="#" class="h5p-previous">Prev</a><a href="#" class="h5p-scroll-left">&lt;</a><ol></ol><a href="#" class="h5p-scroll-right">&gt;</a><a href="#" class="h5p-next">Next</a></div></div>');
+  var $wrapper = $container.children('.h5p-wrapper');
+  var $presentationWrapper = $wrapper.children('.h5p-presentation-wrapper');
+  this.$slidesWrapper = $presentationWrapper.children('.h5p-slides-wrapper');
+  var $keywordsWrapper = $presentationWrapper.children('.h5p-keywords-wrapper');
+  var $slideination = $wrapper.children('.h5p-slideination');
   var keywords = '';
+  var slideinationSlides = '';
   
   for (var i = 0; i < this.slides.length; i++) {
     var slide = this.slides[i];
-    var $slide = H5P.jQuery('<div class="slide"></div>').appendTo(this.$slidesWrapper);
+    var $slide = H5P.jQuery('<div class="h5p-slide"></div>').appendTo(this.$slidesWrapper);
     var first = i === 0;
     
     if (first) {
-      this.$current = $slide.addClass('current');
+      this.$current = $slide.addClass('h5p-current');
     }
     
     this.addElements($slide, slide.elements);
     keywords += this.keywordsHtml(slide.keywords, first);
+    
+    slideinationSlides += '<li' + (first ? ' class="h5p-current"' : '') + '><a href="#">' + (i + 1) + '</a></li>';
   }
   
   this.$slides = this.$slidesWrapper.children();
+  
+  // Initialize keywords
   if (keywords) {
-    this.$keywords.html('<ol>' + keywords + '</ol>');
+    this.$keywords = $keywordsWrapper.html('<ol>' + keywords + '</ol>').children('ol').children('li');
+    this.$currentKeyword = this.$keywords.filter('.h5p-current');
   }
-  this.$currentKeyword = this.$keywords.find('.current');
 
   // Initialize key events
   this.initKeyEvents();
   
   // Initialize touch events
   this.initTouchEvents();
+  
+  // Slideination
+  this.initSlideination($slideination, slideinationSlides);
 };
 
 /**
@@ -100,7 +110,7 @@ H5P.CoursePresentation.prototype.keywordsHtml = function (keywords, first) {
     }
   }
   
-  return '<li' + (first ? ' class="current"' : '') + '>' + html + '</li>';
+  return '<li' + (first ? ' class="h5p-current"' : '') + '>' + html + '</li>';
 };
 
 /**
@@ -147,7 +157,7 @@ H5P.CoursePresentation.prototype.initTouchEvents = function () {
  
   this.$slidesWrapper.bind('touchstart', function (event) {
     // Disable animations when touching
-    that.$slidesWrapper.removeClass('animate');
+    that.$slidesWrapper.removeClass('h5p-animate');
     
     // Set start positions
     startX = event.originalEvent.touches[0].pageX;
@@ -192,7 +202,7 @@ H5P.CoursePresentation.prototype.initTouchEvents = function () {
     
   }).bind('touchend', function () {
     // Enable animations again
-    that.$slidesWrapper.addClass('animate');
+    that.$slidesWrapper.addClass('h5p-animate');
     
     if (!scroll) {
       // If we're not scrolling detemine if we're changing slide
@@ -210,6 +220,57 @@ H5P.CoursePresentation.prototype.initTouchEvents = function () {
   });
 };
 
+H5P.CoursePresentation.prototype.initSlideination = function ($slideination, slideinationSlides) {
+  var that = this;
+  var $ol = $slideination.children('ol');
+  var timer;
+  
+  // Slide selector
+  this.$slideinationSlides = $ol.html(slideinationSlides).children('li').children('a').click(function () {
+    that.jumpToSlide(H5P.jQuery(this).text() - 1);
+    
+    return false;
+  }).end();
+  this.$currentSlideinationSlide = this.$slideinationSlides.filter('.h5p-current');
+  
+  var disableClick = function () {
+    return false;
+  };
+  var scrollLeft = function (event) {
+    event.preventDefault();
+    timer = setInterval(function () {
+      $ol.scrollLeft($ol.scrollLeft() - 1);
+    }, 1);
+  };
+  var scrollRight = function (event) {
+    event.preventDefault();
+    timer = setInterval(function () {
+      $ol.scrollLeft($ol.scrollLeft() + 1);
+    }, 1);
+  };
+  var stopScroll = function () {
+    clearInterval(timer);
+  };
+  
+  // Scroll slide selector to the left
+  $slideination.children('.h5p-scroll-left').click(disableClick).mousedown(scrollLeft).mouseup(stopScroll).bind('touchstart', scrollLeft).bind('touchend', stopScroll);
+  
+  // Scroll slide selector to the right
+  $slideination.children('.h5p-scroll-right').click(disableClick).mousedown(scrollRight).mouseup(stopScroll).bind('touchstart', scrollRight).bind('touchend', stopScroll);
+  
+  // Previous slide button
+  $slideination.children('.h5p-previous').click(function () {
+    that.previousSlide();
+    return false;
+  });
+  
+  // Next slide button
+  $slideination.children('.h5p-next').click(function () {
+    that.nextSlide();
+    return false;
+  });
+};
+
 /**
  * Switch to previous slide
  *
@@ -221,17 +282,7 @@ H5P.CoursePresentation.prototype.previousSlide = function () {
     return false;
   }
   
-  this.$current.removeClass('current');
-  this.$current = $prev.addClass('current').removeClass('previous');
-  
-  // Change keyword
-  $prev = this.$currentKeyword.prev();
-  this.$currentKeyword.removeClass('current');
-  this.$currentKeyword = $prev.addClass('current');
-  
-  this.$currentKeyword.parent().animate({scrollTop: '+=' + (this.$currentKeyword.position().top - 8)}, 250);
-  
-  return true;
+  return this.jumpToSlide($prev.index());
 };
 
 /**
@@ -244,16 +295,50 @@ H5P.CoursePresentation.prototype.nextSlide = function () {
   if (!$next.length) {
     return false;
   }
+
+  return this.jumpToSlide($next.index());
+};
+
+/**
+ * 
+ * @param {int} slideNumber
+ * @returns {undefined}
+ */
+H5P.CoursePresentation.prototype.jumpToSlide = function (slideNumber) {
+  var $parent;
+  var isiPad = navigator.userAgent.match(/iPad/i) !== null;
   
-  this.$current.removeClass('current').addClass('previous');
-  this.$current = $next.addClass('current');
+  // Jump to slide
+  this.$current.removeClass('h5p-current');
+  this.$current = this.$slides.removeClass('h5p-previous').filter(':lt(' + slideNumber + ')').addClass('h5p-previous').end().eq(slideNumber).addClass('h5p-current');
   
-  // Change keyword
-  $next = this.$currentKeyword.next();
-  this.$currentKeyword.removeClass('current');
-  this.$currentKeyword = $next.addClass('current');
+  // Jump keywords
+  if (this.$keywords !== undefined) {
+    this.$currentKeyword.removeClass('h5p-current');
+    this.$currentKeyword = this.$keywords.eq(slideNumber).addClass('h5p-current');
+    
+    if (isiPad) {
+    // scrollTop animations does not work well on ipad.
+    // TODO: Check on iPhone.
+      $parent = this.$currentKeyword.parent();
+      $parent.scrollTop($parent.scrollTop() + this.$currentKeyword.position().top - 8);
+    }
+    else {
+      this.$currentKeyword.parent().stop().animate({scrollTop: '+=' + (this.$currentKeyword.position().top - 8)}, 250);
+    }
+  }
   
-  this.$currentKeyword.parent().animate({scrollTop: this.$currentKeyword.position().top - 8}, 250);
+  // Jump slideination
+  this.$currentSlideinationSlide.removeClass('h5p-current');
+  this.$currentSlideinationSlide = this.$slideinationSlides.eq(slideNumber).addClass('h5p-current');
+  
+  $parent = this.$currentSlideinationSlide.parent();
+  if (isiPad) {
+    $parent.scrollLeft(this.$currentSlideinationSlide.position().left - ($parent.width() / 2) + 17 + $parent.scrollLeft());
+  }
+  else {
+    $parent.animate({scrollLeft: '+=' + (this.$currentSlideinationSlide.position().left - ($parent.width() / 2) + 17)}, 250);
+  }
   
   return true;
 };
