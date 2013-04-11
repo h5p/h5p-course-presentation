5cec78031a41ab8f9b22161437f309587df1403a
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2013-04-11 15:09:17 +0200

    Merge in origin master

--- scripts/cp.js
+++ scripts/cp.js
@@ -8,11 +8,22 @@ var H5P = H5P || {};
  * @returns {undefined} Nothing.
  */
 H5P.CoursePresentation = function (params, id) {
+  this.swipeThreshold = 100; // px
+  
   this.slides = params.slides;
   this.slidesWithSolutions = [];
+  
+  this.l10n = params.l10n !== undefined ? params.l10n : {
+    // Defaults
+    prev: "Prev",
+    prevSlide: "Previous slide",
+    scrollLeft: "Scroll - left",
+    jumpToSlide: "Jump to slide",
+    scrollRight: "Scroll - right",
+    next: "Next",
+    nextSlide: "Next slide"
+  };
   this.contentPath = H5P.getContentPath(id);
-
-  this.ratio = 640 / 400;
 };
 
 /**
@@ -31,11 +42,11 @@ H5P.CoursePresentation.prototype.attach = function ($container) {
     '<div class="h5p-keywords-wrapper"></div>' +
   '</div>' +
   '<div class="h5p-slideination">' +
-    '<a href="#" class="h5p-previous" title="Previous slide">Prev</a>' +
-    '<a href="#" class="h5p-scroll-left" title="Scroll - left">&lt;</a>' +
+    '<a href="#" class="h5p-previous" title="' + this.l10n.prevSlide + '">' + this.l10n.prev + '</a>' +
+    '<a href="#" class="h5p-scroll-left" title="' + this.l10n.scrollLeft + '">&lt;</a>' +
     '<ol></ol>' +
-    '<a href="#" class="h5p-scroll-right" title="Scroll - right">&gt;</a>' +
-    '<a href="#" class="h5p-next" title="Next slide">Next</a>' +
+    '<a href="#" class="h5p-scroll-right" title="' + this.l10n.scrollRight + '">&gt;</a>' +
+    '<a href="#" class="h5p-next" title="' + this.l10n.nextSlide + '">' + this.l10n.next + '</a>' +
   '</div>' +
   '<a href="#" class="h5p-show-solutions" style="display: none;" title="">Show solutions</a>' +
 '</div>';
@@ -55,11 +66,17 @@ H5P.CoursePresentation.prototype.attach = function ($container) {
     }
   });
 
+  this.width = parseInt(this.$wrapper.css('width'));
+  this.height = parseInt(this.$wrapper.css('height'));
+  this.ratio = this.width / this.height;
+  this.fontSize = parseInt(this.$wrapper.css('fontSize'));
+
   this.$presentationWrapper = this.$wrapper.children('.h5p-presentation-wrapper');
   this.$slidesWrapper = this.$presentationWrapper.children('.h5p-slides-wrapper');
   var $keywordsWrapper = this.$presentationWrapper.children('.h5p-keywords-wrapper');
-  var $slideination = this.$wrapper.children('.h5p-slideination');
+  this.$slideination = this.$wrapper.children('.h5p-slideination');
   var $solutionsButton = this.$wrapper.children('.h5p-show-solutions');
+
   var keywords = '';
   var slideinationSlides = '';
 
@@ -74,21 +91,24 @@ H5P.CoursePresentation.prototype.attach = function ($container) {
 
     this.addElements(i, $slide, slide.elements);
     keywords += this.keywordsHtml(slide.keywords, first);
-
-    slideinationSlides += H5P.CoursePresentation.createSlideinationSlide(i + 1, first);
+    slideinationSlides += H5P.CoursePresentation.createSlideinationSlide(i + 1, this.l10n.jumpToSlide, first);
   }
 
   // Initialize keywords
   if (keywords) {
     this.$keywords = $keywordsWrapper.html('<ol>' + keywords + '</ol>').children('ol');
     this.$currentKeyword = this.$keywords.children('.h5p-current');
+    this.keywordsWidth = $keywordsWrapper.width() / (this.width / 100); // %
+  }
+  else {
+    this.keywordsWidth = 0;
   }
 
   // Initialize touch events
   this.initTouchEvents();
 
   // Slideination
-  this.initSlideination($slideination, slideinationSlides);
+  this.initSlideination(this.$slideination, slideinationSlides);
 
   $solutionsButton.click(function(event) {
     that.showSolutions();
@@ -102,6 +122,11 @@ H5P.CoursePresentation.prototype.attach = function ($container) {
 };
 
 H5P.CoursePresentation.prototype.resize = function (fullscreen) {
+  var fullscreenOn = H5P.$body.hasClass('h5p-fullscreen') || H5P.$body.hasClass('h5p-semi-fullscreen');
+  if (!fullscreenOn) {
+    this.$container.css('height', '99999px');
+  }
+  
   var width = this.$container.width();
   var height = this.$container.height();
 
@@ -118,11 +143,14 @@ H5P.CoursePresentation.prototype.resize = function (fullscreen) {
   this.$wrapper.css({
     width: width + 'px',
     height: height + 'px',
-    fontSize: (16 * (width / 640)) + 'px'
+    fontSize: (this.fontSize * (width / this.width)) + 'px'
   });
 
   if (fullscreen) {
     this.$wrapper.focus();
+  }
+  if (!fullscreenOn) {
+    this.$container.css('height', '100%');
   }
 };
 
@@ -141,7 +169,7 @@ H5P.CoursePresentation.prototype.addElements = function (slideIndex, $slide, ele
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
     var elementInstance = new (H5P.classFromName(element.action.library.split(' ')[0]))(element.action.params, this.contentPath);
-    var $h5pElementContainer = $('<div class="h5p-element" style="left: ' + parseInt(element.x) + 32.8125 + '%; top: ' + element.y + '%; width: ' + element.width + '%; height: ' + element.height + '%;"></div>').appendTo($slide);
+    var $h5pElementContainer = $('<div class="h5p-element" style="left: ' + parseInt(element.x) + '%; top: ' + element.y + '%; width: ' + element.width + '%; height: ' + element.height + '%;"></div>').appendTo($slide);
     elementInstance.attach($h5pElementContainer);
     if (this.hasSolutions(elementInstance)) {
       if (typeof this.slidesWithSolutions[slideIndex] === 'undefined') {
@@ -297,10 +325,10 @@ H5P.CoursePresentation.prototype.initTouchEvents = function () {
     if (!scroll) {
       // If we're not scrolling detemine if we're changing slide
       var moved = startX - lastX;
-      if (moved > 100) {
+      if (moved > that.swipeThreshold) {
         that.nextSlide();
       }
-      else if (moved < -100) {
+      else if (moved < -that.swipeThreshold) {
         that.previousSlide();
       }
     }
@@ -500,13 +528,12 @@ H5P.CoursePresentation.createSlide = function (slide) {
  * @param {int} first Optional
  * @returns {String}
  */
-H5P.CoursePresentation.createSlideinationSlide = function (index, first) {
+H5P.CoursePresentation.createSlideinationSlide = function (index, title, first) {
   var html =  '<li class="h5p-slide-button';
-
   if (first !== undefined && first) {
     html += ' h5p-current';
   }
-  html += '"><a href="#" title="Jump to slide">';
+  html += '"><a href="#" title="' + title + '">';
 
   if (index !== undefined) {
     html += index;
