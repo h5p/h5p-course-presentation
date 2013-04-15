2850faf74bcfce101c82d50d921b0bf0f4ec66a4
Svein-Tore Griff With <falcon@530912.no-reply.drupal.org>
2013-04-15 18:48:05 +0200

    Fix better handling of defaults, add close button to popup and make handling of editor function in cp.js nicer

--- scripts/cp.js
+++ scripts/cp.js
@@ -5,16 +5,19 @@ var H5P = H5P || {};
  *
  * @param {object} params Start paramteres.
  * @param {int} id Content identifier
+ * @param {function} editor
+ *  Set if an editor is initiating this library
  * @returns {undefined} Nothing.
  */
-H5P.CoursePresentation = function (params, id) {
+H5P.CoursePresentation = function (params, id, editor) {
   this.swipeThreshold = 100; // px
 
   this.slides = params.slides;
   this.slidesWithSolutions = [];
+  this.editor = editor;
 
-  this.l10n = params.l10n !== undefined ? params.l10n : {
-    // Defaults
+  this.l10n = params.l10n !== undefined ? params.l10n : {};
+  this.l10n = H5P.jQuery.extend({}, this.l10n, {
     prev: "Prev",
     prevSlide: "Previous slide",
     scrollLeft: "Scroll - left",
@@ -29,8 +32,9 @@ H5P.CoursePresentation = function (params, id) {
     okScore: "Nice effort! You got @percent correct!",
     badScore: "You need to work more on this. You only got @percent correct...",
     total: "TOTAL",
-    showSolutions: "Show solutions"
-  };
+    showSolutions: "Show solutions",
+    close: "Close"
+  });
   this.contentPath = H5P.getContentPath(id);
 };
 
@@ -191,12 +195,12 @@ H5P.CoursePresentation.prototype.addElements = function (slideIndex, $slide, ele
 
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
-    
+
     var elementInstance = new (H5P.classFromName(element.action.library.split(' ')[0]))(element.action.params, this.contentPath);
-    
+
     var $elementContainer = H5P.jQuery('<div class="h5p-element" style="left: ' + (element.x + this.keywordsWidth) + '%; top: ' + element.y + '%; width: ' + (element.width * this.slideWidthRatio) + '%; height: ' + element.height + '%;"></div>').appendTo($slide);
     elementInstance.attach($elementContainer);
-    
+
     if (this.editor !== undefined) {
       // If we're in the H5P editor, allow it to manipulate the elements
       this.editor.processElement(slideIndex, element, elementInstance, $elementContainer);
@@ -204,7 +208,7 @@ H5P.CoursePresentation.prototype.addElements = function (slideIndex, $slide, ele
     else if (element.solution) {
       this.addElementSolutionButton(element, elementInstance, $elementContainer);
     }
-    
+
     if (this.checkForSolutions(elementInstance)) {
       if (this.slidesWithSolutions[slideIndex] === undefined) {
         this.slidesWithSolutions[slideIndex] = [];
@@ -216,7 +220,7 @@ H5P.CoursePresentation.prototype.addElements = function (slideIndex, $slide, ele
 
 /**
  * Adds a solution button?
- * 
+ *
  * @param {Object} element Properties from params.
  * @param {Object} elementInstance Instance of the element.
  * @param {jQuery} $elementContainer Wrapper for the element.
@@ -238,15 +242,17 @@ H5P.CoursePresentation.prototype.addElementSolutionButton = function (element, e
 
 /**
  * Displays a popup.
- * 
+ *
  * @param {String} popupContent
  * @returns {undefined}
  */
 H5P.CoursePresentation.prototype.showPopup = function (popupContent) {
-  var html = '<div class="h5p-popup-overlay"><div class="h5p-popup-container">' + popupContent + '</div></div>';
-  H5P.jQuery(html).prependTo(this.$container).click(function() {
-    H5P.jQuery(this).remove();
-  });
+  var $popup = H5P.jQuery('<div class="h5p-popup-overlay"><div class="h5p-popup-container">' + popupContent +
+           '<a href="#" class="h5p-button h5p-close-popup">' + this.l10n.close + '</a></div></div>')
+  .prependTo(this.$container).find('.h5p-close-popup').click(function(event) {
+    event.preventDefault();
+    $popup.remove();
+  }).end();
 };
 
 /**
@@ -590,7 +596,7 @@ H5P.CoursePresentation.createSlide = function (slide) {
   return '<div class="h5p-slide"' + (slide.background !== undefined ? ' style="background:' + slide.background + '"' : '') + '"></div>';
 };
 
-/** 
+/**
  * Create html for a slideination slide.
  *
  * @param {int} index Optional
@@ -615,7 +621,7 @@ H5P.CoursePresentation.createSlideinationSlide = function (index, title, first) 
 
 /**
  * Show solutions for all slides that have solutions
- * 
+ *
  * @returns {undefined}
  */
 H5P.CoursePresentation.prototype.showSolutions = function () {
