e73b255f9b758bf938b1ef0411d2218624963fbd
Svein-Tore Griff With <s.t.g.with@gmail.com>
2015-03-27 00:12:43 +0100

    subContentId instead of uuid

--- upgrades.js
+++ upgrades.js
@@ -76,10 +76,10 @@ H5PUpgrades['H5P.CoursePresentation'] = (function ($) {
         var slides = parameters.presentation.slides;
         for (var i = 0; i < slides.length; i++) {
           for (var j = 0; j < slides[i].elements.length; j++) {
-            if (slides[i].elements[j].action.uuid === undefined) {
+            if (slides[i].elements[j].action.subContentId === undefined) {
               // NOTE: We avoid using H5P.createUUID since this is an upgrade script and H5P function may change in the
               // future
-              slides[i].elements[j].action.uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
+              slides[i].elements[j].action.subContentId = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(char) {
                 var random = Math.random()*16|0, newChar = char === 'x' ? random : (random&0x3|0x8);
                 return newChar.toString(16);
               });
