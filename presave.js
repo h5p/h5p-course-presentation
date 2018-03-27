b4df5e452a47a222151335dc260257089d13918a
Thomas Horn Sivertsen <thomass@cerpus.com>
2018-03-27 16:20:34 +0200

    Added/modified calculation of max score

--- presave.js
+++ presave.js
@@ -1,62 +1,61 @@
 var H5PPresave = H5PPresave || {};
+var H5PEditor = H5PEditor || {};
 
 
 /**
- * Function to go through all elements of a Course Presentation and perform the separate calculations before returning a aggregated result
+ * Function to go thr all elements of a Course Presentation and perform the separate calculations before returning a aggregated result
  *
  * @param content
  * @param finished
  * @constructor
  */
 H5PPresave['H5P.CoursePresentation'] = function (content, finished) {
-    var self = this;
-    if( typeof content === "undefined" || !content.hasOwnProperty('presentation') || !content.presentation.hasOwnProperty('slides')){
-        throw {
-            name: 'Invalid Course Presentation Error',
-            message: "Could not find expected semantics in content."
-        };
-    }
+  var presave = H5PEditor.Presave;
 
-    var score = Object
-        .keys(content.presentation.slides)
-        .map(function (value, index) {
-            var slide = content.presentation.slides[index];
-            if( !slide.hasOwnProperty('elements')){
-                return [];
-            }
-            return slide.elements;
-        })
-        .filter(function(elements){
-            return elements.length > 0;
-        })
-        .reduce(function (previous, current){
-            return previous.concat(current);
-        }, [])
-        .map(function (element) {
-            if( element.hasOwnProperty('action')){
-                return element.action;
-            }
-            return {};
-        })
-        .filter(function (action) {
-            return action.hasOwnProperty('library') && action.hasOwnProperty('params');
-        })
-        .map(function (action) {
-            return (new self.constructor).process(action.library, action.params).getMaxScore();
-        })
-        .reduce(function(currentScore, scoreToAdd){
-            if( self.isInt(scoreToAdd)){
-                currentScore += scoreToAdd;
-            }
-            return currentScore;
-        }, 0);
+  if (isContentInValid()) {
+    throw new presave.exceptions.InvalidContentSemanticsException('Invalid Course Presentation Error')
+  }
 
-    if( isNaN(score) || score < 0){
-        throw {
-            name: 'InvalidMaxScore Error',
-            message: "Could not calculate the max score for this content. The max score is assumed to be 0. Contact your administrator if this isn’t correct."
-        };
-    }
+  var score = content.presentation.slides
+    .map(function (value, index) {
+      var slide = content.presentation.slides[index];
+      if (!slide.hasOwnProperty('elements')) {
+        return [];
+      }
+      return slide.elements;
+    })
+    .filter(function (elements) {
+      return elements.length > 0;
+    })
+    .reduce(function (previous, current) {
+      return previous.concat(current);
+    }, [])
+    .map(function (element) {
+      if (element.hasOwnProperty('action')) {
+        return element.action;
+      }
+      return {};
+    })
+    .filter(function (action) {
+      return action.hasOwnProperty('library') && action.hasOwnProperty('params');
+    })
+    .map(function (action) {
+      return (new presave).process(action.library, action.params).maxScore;
+    })
+    .reduce(function (currentScore, scoreToAdd) {
+      if (presave.isInt(scoreToAdd)) {
+        currentScore += scoreToAdd;
+      }
+      return currentScore;
+    }, 0);
 
+  presave.validateScore(score);
+
+  if (finished) {
     finished({maxScore: score});
+  }
+
+  function isContentInValid() {
+    return !presave.checkNestedRequirements(content, 'content.presentation.slides') || !Array.isArray(content.presentation.slides);
+  }
 };
