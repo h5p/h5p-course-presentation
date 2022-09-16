61b217248f04566197b8261402e98f5bb07e6ea6
Paal Joergensen <pal.jorgensen@gmail.com>
2022-09-16 09:56:13 +0200

    Fix placement for summary+fullscreen tooltip

--- src/scripts/navigation-line.js
+++ src/scripts/navigation-line.js
@@ -132,13 +132,15 @@ const NavigationLine = (function ($) {
 
       this.progresbarKeyboardControls.addElement($link.get(0));
 
-      // Add hover effect if not an ipad or iphone.
-      if (!isIOS) {
-        H5P.Tooltip($li.get(0));
-      }
-
+      let tooltipPosition = 'top';
       if (this.isSummarySlide(i)) {
         $li.addClass('progressbar-part-summary-slide');
+        tooltipPosition = 'left';
+      }
+
+      // Add hover effect if not an ipad or iphone.
+      if (!isIOS) {
+        H5P.Tooltip($li.get(0), {position: tooltipPosition});
       }
 
 
@@ -385,7 +387,7 @@ const NavigationLine = (function ($) {
           'tabindex': '0'
         });
 
-        H5P.Tooltip(this.cp.$fullScreenButton.get(0));
+        H5P.Tooltip(this.cp.$fullScreenButton.get(0), {position: 'left'});
 
         addClickAndKeyboardListeners(this.cp.$fullScreenButton, () => that.cp.toggleFullScreen());
 
